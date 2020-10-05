04272d03e524ea3d7b64991be139b5e02213e53c
BrunoIfrit <64979873+BrunoIfrit@users.noreply.github.com>
2020-10-05 16:16:47 -0300

    Add files via upload

--- Jogo_snake/script.js
+++ Jogo_snake/script.js
@@ -8,6 +8,17 @@ snake[0] = {
 }
 
 let direction = "";
+let food = {
+    x: Math.floor(Math.random() * 15 +1)* box,
+    y: Math.floor(Math.random() * 15 +1)* box
+}
+
+function drawFood(){
+ context.fillStyle = " red";
+ context.fillRect(food.x,food.y,box,box);
+
+
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -30,8 +41,7 @@ function update(event) {
     if (event.keyCode == 40 && direction != "down") direction = "up"
     if (event.keyCode == 39 && direction != "left") direction = "right"
     if (event.keyCode == 38 && direction != "up") direction = "down"
-
-    console.log(direction);
+     
 }
 
 function inciarjogo(){
@@ -40,8 +50,17 @@ function inciarjogo(){
    if(snake[0].y > 15*box && direction == "up") snake[0].y = 0;
    if(snake[0].y < 0 && direction == "down") snake[0].y = 16* box;
 
+   for(i = 1; i < snake.length ; i++){
+       if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+           clearInterval(jogo);
+           alert('Game Over :(');
+       }
+   }
+
+
  criarBG();
  criarCobrinha();
+ drawFood();
  
  let snakeX = snake[0].x;
  let snakeY = snake[0].y;
@@ -51,7 +70,11 @@ function inciarjogo(){
  if(direction == "up"){snakeY += box;}
  if(direction == "down"){snakeY -= box;}
 
- snake.pop();
+ if(snakeX != food.x || snakeY != food.y){
+    snake.pop();
+   } else {food.x = Math.floor(Math.random() * 15 +1)* box;
+        food.y =  Math.floor(Math.random() * 15 +1)* box
+   }
 
  let newHead = {
      x: snakeX,
